454c36870548c4f1b28d9f0b8133b05edfa44df6
TysonEmporer786 <134965478+TysonEmporer786@users.noreply.github.com>
2023-05-30 03:03:30 +0530

    Update luffu.py

--- luffu.py
+++ luffu.py
@@ -12,10 +12,10 @@ bot_token = '6237491510:AAFiSFLCzra8-3Ma4T9fju-Zwhffg7cGeJQ'
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Handler function for the /start command
-@Client.on_message(filters.command("start"))
+@app.on_message(filters.command("start"))
 def start_command(client: Client, message: Message):
     # Reply to the user with a welcome message
     message.reply_text("Welcome to my bot! How can I assist you?")
 
 # Start the bot
-bot.run()
+app.run()
