3673f02b1bdcd3f1e4acfac64fd287d4c86ae3af
TysonEmporer786 <134965478+TysonEmporer786@users.noreply.github.com>
2023-05-30 02:24:03 +0530

    Update luffu.py

--- luffu.py
+++ luffu.py
@@ -3,7 +3,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Create a new Pyrogram client
-bot = Client("your_bot")
+api_id = 20693296
+
+api_hash = 2c662827e953e4b8f3ca2f8695694dce
+
+bot_token = 6237491510:AAFiSFLCzra8-3Ma4T9fju-Zwhffg7cGeJQ
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Handler function for the /start command
 @Client.on_message(filters.command("start"))
